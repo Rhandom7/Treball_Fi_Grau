ebc96e1020f07c52923e4e93c88c378d5c51f048
Genís <genisarno7@gmail.com>
2019-05-16 19:45:02 +0200

    Implementat permís ubicació i activar ubicació i bluetooth

--- app/src/main/java/com/example/orientacioeps/Activity/MainActivity.java
+++ app/src/main/java/com/example/orientacioeps/Activity/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.orientacioeps.Activity;
 
-
-
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
 import android.content.Intent;
-
 
 import android.view.View;
 import android.widget.AdapterView;
@@ -27,9 +33,22 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
-    // Declare Variables
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ResultCallback {
+
+    //Variables
     TodoApi mTodoService;
     ListView list;
     ListViewAdapter adapter;
@@ -37,27 +56,58 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
 
     List<Espai> llistaEspais = new ArrayList<>();
 
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+    private static final int PERMISSION_REQUEST_COARSE_BL = 2;
+
+    protected GoogleApiClient mGoogleApiClient;
+    protected LocationRequest locationRequest;
+    int REQUEST_CHECK_SETTINGS = 100;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Locate the ListView in activity_main.xml
-        list = (ListView) findViewById(R.id.listview);
+        list = findViewById(R.id.listview);
 
         mTodoService = ((TodoApp)this.getApplication()).getAPI();
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(LocationServices.API).addConnectionCallbacks(this).addOnConnectionFailedListener(this).build();
+        mGoogleApiClient.connect();
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(30 * 1000);
+        locationRequest.setFastestInterval(5 * 1000);
+
+        initializeLocation();
+        initializeBluetooth();
+
+        obtenirEspais();
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i =new Intent(MainActivity.this, EspaiSeleccionat.class);
+                i.putExtra("Element", llistaEspais.get(position).nom);
+                MainActivity.this.startActivity(i);
+            }
+        });
+    }
+
+    private void obtenirEspais(){
         Call<List<Espai>> call = mTodoService.getEspais();
 
         call.enqueue(new Callback<List<Espai>>() {
             @Override
             public void onResponse(Call<List<Espai>> call, Response<List<Espai>> response) {
                 if (response.isSuccessful()) {
-                    for(Espai e : response.body()){
-                        llistaEspais.add(e);
-                    }
+                    llistaEspais.addAll(response.body() != null ? response.body() : llistaEspais);
                     adapter = new ListViewAdapter(MainActivity.this, llistaEspais);
                     list.setAdapter(adapter);
-                    editsearch = (SearchView) findViewById(R.id.search);
+                    editsearch = findViewById(R.id.search);
                     editsearch.setOnQueryTextListener(MainActivity.this);
                 } else {
                     Toast toast = Toast.makeText(MainActivity.this, "Error intentant obtenir els espais", Toast.LENGTH_SHORT);
@@ -70,27 +120,6 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
                 toast.show();
             }
         });
-
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i =new Intent(MainActivity.this, EspaiSeleccionat.class);
-                i.putExtra("Element", llistaEspais.get(position).nom);
-                MainActivity.this.startActivity(i);
-            }
-        });
-
-
-        /*final TextView espaiSeleccionat = (TextView)findViewById(R.id.espai);
-
-        espaiSeleccionat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(MainActivity.this, espaiSeleccionat.getText().toString(), Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });*/
-
     }
 
     @Override
@@ -100,8 +129,161 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
 
     @Override
     public boolean onQueryTextChange(String newText) {
-        String text = newText;
-        adapter.filter(text);
+        adapter.filter(newText);
         return false;
+    }
+
+    private void initializeBluetooth(){
+        //Check if device does support BT by hardware
+        if (!getBaseContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
+            //Toast shows a message on the screen for a LENGTH_SHORT period
+            Toast.makeText(this, "BLUETOOTH NOT SUPPORTED!", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        //Check if device does support BT Low Energy by hardware. Else close the app(finish())!
+        if (!getBaseContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            //Toast shows a message on the screen for a LENGTH_SHORT period
+            Toast.makeText(this, "BLE NOT SUPPORTED!", Toast.LENGTH_SHORT).show();
+            finish();
+        }else {
+            //If BLE is supported, get the BT adapter. Preparing for use!
+            BluetoothAdapter mBTAdapter = BluetoothAdapter.getDefaultAdapter();
+            //If getting the adapter returns error, close the app with error message!
+            if (mBTAdapter == null) {
+                Toast.makeText(this, "ERROR GETTING BLUETOOTH ADAPTER!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            else{
+                //Check if BT is enabled! This method requires BT permissions in the manifest.
+                if (!mBTAdapter.isEnabled()) {
+                    //If it is not enabled, ask user to enable it with default BT enable dialog! BT enable response will be received in the onActivityResult method.
+                    Intent enableBTintent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBTintent, PERMISSION_REQUEST_COARSE_BL);
+                }
+            }
+        }
+    }
+
+    private void initializeLocation(){
+        //If Android version is M (6.0 API 23) or newer, check if it has Location permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+            }
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //Check if permission request response is from Location
+        // If request is cancelled, the result arrays are empty.
+        if (requestCode == PERMISSION_REQUEST_COARSE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // permission was granted, yay! Do the location-related task you need to do.
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                }
+            } else {
+                // permission denied, boo! Disable the functionality that depends on this permission.
+                dialogLocationPermission();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CHECK_SETTINGS || requestCode == PERMISSION_REQUEST_COARSE_BL) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(getApplicationContext(), "Bluetooth i/o Localització activat", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Bluetooth i/o Localització no activat", Toast.LENGTH_LONG).show();
+                dialogLocationBluetooth();
+                //finish();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {}
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {}
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+        PendingResult result = LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build());
+        result.setResultCallback(this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onResult(@NonNull Result result) {
+        final Status status = result.getStatus();
+        switch (status.getStatusCode()) {
+            case LocationSettingsStatusCodes.SUCCESS:
+                // NO need to show the dialog;
+                break;
+            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                //  Location settings are not satisfied. Show the user a dialog
+                try {
+                    // Show the dialog by calling startResolutionForResult(), and check the result in onActivityResult().
+                    status.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTINGS);
+                } catch (IntentSender.SendIntentException e) {
+                    //failed to show
+                }
+                break;
+            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                // Location settings are unavailable so not possible to show any dialog now
+                break;
+        }
+    }
+
+    private void dialogLocationBluetooth(){
+        new AlertDialog.Builder(this)
+            .setMessage(R.string.dialog_Location_Bluetooth)
+            .setPositiveButton(R.string.dialog_accept, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    //Prompt the user once explanation has been shown
+                }
+            })
+            .create()
+            .show();
+    }
+
+    private void dialogLocationPermission(){
+        new AlertDialog.Builder(this)
+            .setMessage(R.string.dialog_Location_Permission)
+            .setPositiveButton(R.string.dialog_accept, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    //Prompt the user once explanation has been shown
+                }
+            })
+            .create()
+            .show();
     }
 }
